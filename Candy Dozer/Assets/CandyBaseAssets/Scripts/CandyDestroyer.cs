d7dc42c884e2ad09029f65015b3207c81eb8a2e6
shinya yuta <e145742@shinyayuuta-no-MacBook-Pro.local>
2016-11-10 17:30:51 +0900

    Unity_file

---
+++ Candy Dozer/Assets/CandyBaseAssets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyDestroyer : MonoBehaviour {
+
+	public CandyHolder candyHolder;
+	public int reward;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Candy") {
+
+			candyHolder.AddCandy (reward);
+
+			Destroy (other.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	/*
+	 * void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	*/
+}
